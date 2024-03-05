576214c3d020cd9d95c2641140c9ae0d00feed1b
koskab <koskab13@icloud.com>
2024-03-05 17:22:35 +0600

    First commit

---
+++ src/main/java/com/test/trysecurity/core/entity/AuditEntity.java
@@ -0,0 +1,4 @@
+package com.test.trysecurity.core.entity;
+
+public class AuditEntity {
+}
---
+++ src/test/java/com/test/trysecurity/TrysecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.trysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrysecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
